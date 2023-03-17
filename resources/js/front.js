eb9b30c6ca52e4f58d22e035937d30ca579de701
Mikhail Abramishvili <mg@abramishvili.net>
2023-03-17 20:09:43 +0400

    2023-03-17

---
+++ resources/js/front.js
@@ -0,0 +1,9 @@
+import './bootstrap'
+
+import { createApp } from 'vue'
+
+import App from './components/front/App.vue'
+
+const app = createApp(App)
+
+app.mount('#front')
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('front');
+});
+
+// ADMIN
+Route::get('admin', function () {
+    return view('admin');
+})->middleware(['auth']);
+
+Route::prefix("admin")->middleware(['auth'])->group(function() {
+    Route::get('{any}', function () {
+        return view('admin');
+    })->where('any', '.*');
+});
+
+require __DIR__.'/auth.php';
